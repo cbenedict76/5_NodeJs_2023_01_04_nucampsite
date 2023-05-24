3d402638e03842247f77821b555189b476c5d4dc
Corey Benedict-Osborn <corey.benedict@gmail.com>
2023-05-23 21:02:33 -0400

    5-NodeJS: Week 4: Passport Facebook

--- models/user.js
+++ models/user.js
@@ -3,6 +3,9 @@ const passportLocalMongoose = require('passport-local-mongoose')
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    facebookId: {
+        type: String,
+    },
     firstname: {
         type: String,
         default: ''
